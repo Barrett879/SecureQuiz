fa3940214d32e6b8e424ad07f88d14b7e4f065bf
Barrett879 <77691102+Barrett879@users.noreply.github.com>
2021-05-02 17:04:19 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -16,16 +16,27 @@ def submit():
     session["answer1"] = request.form["answer"]
     return render_template('page1.html')
 
+@app.route('/submit1',methods=['POST'])
+def submit1():
+    session["answer2"] = request.form["answer"]
+    return render_template('page2.html')
+
+@app.route('/submit2',methods=['POST'])
+def submit2():
+    session["answer3"] = request.form["answer"]
+    return render_template('page3.html')
 
 @app.route('/check')
 def check():
     counter = 0
     if session["answer1"] == "A":
         counter = counter + 1
+    if session["answer2"] == "B":
+        counter = counter + 1
+    if session["answer3"] == "C":
+        coutner = counter + 1
                
-               
-    
-               
+         
 
 if __name__=="__main__":
     app.run(debug=False)
